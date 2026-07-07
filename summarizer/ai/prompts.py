ac43b0dff69ace25a376e81ef8049485369a626f
Parisa-Reza <rezaparisa5@gmail.com>
2026-07-07 18:56:36 +0600

    feat: integrating django with langchain

--- summarizer/ai/prompts.py
+++ summarizer/ai/prompts.py
@@ -1,18 +1,30 @@
 from langchain_core.prompts import ChatPromptTemplate
 
-summary_prompt = ChatPromptTemplate.from_template(
-"""
-You are an expert technical writer. But you are also a good teacher. You will summarize the text below in a way that is easy to understand for a beginner in a very easy and simple language
+summary_prompt = ChatPromptTemplate.from_messages(
 
-Summarize the following text.
+    [
+        ("system",
+        """
+        You are an expert technical writer. But you are also a good teacher. You will summarize the text below in a way that is easy to understand for a beginner in a very easy and simple language
+        """
+        ),
+        (
+            "human",
+            """
+            Summarize the following text.
 
-Text:
-{text}
+            Text:
+            {text}
 
-Rules:
-- Maximum 70 words
-- Easy to understand
-- Keep the important points
-- Greet and encourage the reader to learn more about the topic
-"""
+            Rules:
+            - Maximum 70 words
+            - Easy to understand
+            - Keep the important points
+            - Greet and encourage the reader to learn more about the topic
+            """
+        )
+
+        
+    ]
+
 )
